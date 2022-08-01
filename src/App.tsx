31ce3dade746dc759a259760262a857900aae3e9
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-08-01 17:28:23 -0300

    starter the project & fetch jobs API

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  async function fetchJobs() {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  return <h2>tabs project setup</h2>;
+}
+
+export default App;
